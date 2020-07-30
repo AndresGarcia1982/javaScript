1a92d7ac99ee5c43f27a6b00dc9e1cc9a5dd3c2c
AndresGarcia1982 <54553973+AndresGarcia1982@users.noreply.github.com>
2020-07-30 18:19:23 -0500

    Inicio

---
+++ js/app.js
@@ -0,0 +1,123 @@
+//definir variable calculadora
+var calculadora = {
+  pantalla: document.getElementById("display"),
+  valorPantalla: "0",
+  operacion: "",
+  primerValor: 0,
+  segundoValor: 0,
+  finalValor: 0,
+  resultado: 0,
+  auxTeclaIgual: false,
+
+  init: (function (){
+    this.asignarEventosBotones(".tecla");
+    this.asignarEventosFuncion();
+  }),
+
+  //Eventos con los botones de la calculadora
+  asignarEventosBotones: function(selector){
+    var x = document.querySectorAll(selector);
+    for (var i = 0; i<x.length; i++){
+      x[i].onmouseover = this.eventoReduceBoton;
+      x[i].onmouseleave = this.eventoRegresaBoton;
+    };
+  },
+
+  eventoReduceBoton: function(event){
+    calculadora.reduceBoton(event.target);
+  },
+  eventoRegresaBoton: function(event){
+    calculadora.aumentoBoton(event.target);
+  },
+
+  //Formato de los Botones
+  reduceBoton: function(elemento){
+    var x = elemento.id;
+    if (x=="1" || x=="2" || x=="3" || x=="0" || x=="igual" || x=="punto"){
+      elemento.style.width = "28%";
+      elemento.style.height = "62px";
+    }else if (x=="signoMas") {
+      elemento.style.width = "88%";
+      elemento.style.height = "98%";
+    }else {
+      elemento.style.width = "21%";
+      elemento.style.height = "62px";
+    }
+  },
+
+  aumentoBoton: function(elemento){
+    var x = elemento.id;
+    if (x=="1" || x=="2" || x=="3" || x=="0" || x=="igual" || x=="punto"){
+      elemento.style.width = "29%";
+      elemento.style.height = "62.91px";
+    }else if (x=="signoMas") {
+      elemento.style.width = "90%";
+      elemento.style.height = "100%";
+    }else {
+      elemento.style.width = "22%";
+      elemento.style.height = "62.91px";
+    }
+  },
+
+  //Asignar eventos en la calculadora
+  asignarEventosFuncion: function(){
+		document.getElementById("0").addEventListener("click", function() {calculadora.ingresoNumero("0");});
+		document.getElementById("1").addEventListener("click", function() {calculadora.ingresoNumero("1");});
+		document.getElementById("2").addEventListener("click", function() {calculadora.ingresoNumero("2");});
+		document.getElementById("3").addEventListener("click", function() {calculadora.ingresoNumero("3");});
+		document.getElementById("4").addEventListener("click", function() {calculadora.ingresoNumero("4");});
+		document.getElementById("5").addEventListener("click", function() {calculadora.ingresoNumero("5");});
+		document.getElementById("6").addEventListener("click", function() {calculadora.ingresoNumero("6");});
+		document.getElementById("7").addEventListener("click", function() {calculadora.ingresoNumero("7");});
+		document.getElementById("8").addEventListener("click", function() {calculadora.ingresoNumero("8");});
+		document.getElementById("9").addEventListener("click", function() {calculadora.ingresoNumero("9");});
+		document.getElementById("on").addEventListener("click", function() {calculadora.borrarDisplay();});
+		document.getElementById("sign").addEventListener("click", function() {calculadora.cambiarSigno();});
+		document.getElementById("punto").addEventListener("click", function() {calculadora.ingresoDecimal();});
+		document.getElementById("igual").addEventListener("click", function() {calculadora.verResultado();});
+		document.getElementById("raiz").addEventListener("click", function() {calculadora.ingresoOperacion("raiz");});
+		document.getElementById("dividido").addEventListener("click", function() {calculadora.ingresoOperacion("/");});
+		document.getElementById("por").addEventListener("click", function() {calculadora.ingresoOperacion("*");});
+		document.getElementById("menos").addEventListener("click", function() {calculadora.ingresoOperacion("-");});
+		document.getElementById("mas").addEventListener("click", function() {calculadora.ingresoOperacion("+");});
+	},
+
+  borrarPantalla: function() {
+    this.valorPantalla = "0";
+    this.operacion = "";
+    this.primerValor = 0;
+    this.segundoValor = 0;
+    this.finalValor = 0;
+    this.resultado = 0;
+    this.auxTeclaIgual = false;
+    this.actualizaPantalla();
+  },
+
+  cambiarSigno: function(){
+    if (this.valorPantalla != "0"){
+      var aux;
+      if (this.valorPantalla.charAt(0)=="-"){
+        aux = this.valorPantalla.slice(1);
+      }else {
+        aux = "-" + this.valorPantalla;
+      }
+    this.valorPantalla = "";
+    this.valorPantalla = aux;
+    this.actualizaPantalla();
+    }
+  },
+
+  ingresoDecimal: function(){
+    if (this.valorPantalla.indexOf(".")== -1){
+      if (this.valorPantalla == ""){
+        this.valorPantalla = this.valorPantalla + "0.";
+      }else {
+        this.valorPantalla = this.valorPantalla + ".";
+      }
+      this.actualizaPantalla();
+    }
+  },
+
+
+
+}
